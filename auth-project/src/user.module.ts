5aa57a94d6dfe4e12a06992fa04013d8d4cee159
TimurAbdullin <t.abdullin@hightech.group>
2021-07-26 14:37:30 +0300

    added typeorm template

---
+++ auth-project/src/user.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from './database/database.module';
+import { userProviders } from './database/user.providers';
+import { UserService } from './user.service';
+
+@Module({
+  imports: [DatabaseModule],
+  providers: [...userProviders, UserService],
+})
+export class UserModule {}
