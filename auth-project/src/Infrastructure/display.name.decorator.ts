20c7f696038fa092f9474892cb425229fd89e47a
Arthur Zagitov <a.zagitov@hightech.group>
2021-07-27 18:09:36 +0300

    deleted

--- auth-project/src/Infrastructure/display.name.decorator.ts
+++
@@ -1,7 +0,0 @@
-import { PropertyMetadata } from "../Models/property.metadata";
-
-export function DisplayName(name: string) : PropertyDecorator{
-  return function(target: any, propertyKey: string){
-    target.constructor.properties.push(new PropertyMetadata(propertyKey, name));
-  }
-}
--- graphql-auth-react/src/AuthHook.ts
+++
@@ -1,8 +0,0 @@
-import {useContext} from "react";
-import {useLocation} from "react-router";
-import {useLocalStorage} from "./LocalStorageHook";
-
-export function useJwt() {
-    let [jwt, setJwt] = useLocalStorage("jwt", '');
-    return [jwt, setJwt];
-}
